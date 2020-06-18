0e6439f8930c7d78cabdd473a45c8363cc9ca19b
unknown <abdulrahman312@yahoo.com>
2020-06-18 23:19:58 +0300

    Update UIController and index.html

--- app.js
+++ app.js
@@ -11,17 +11,40 @@ var budgetController = (function() {
 // UI Controller
 var UIController = (function() {
 
-    
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputValue: '.add__value',
+        inputDescription: '.add__description',
+        inputBtn: '.add__btn'
+    };
 
+    return {
+        getinput: function() {
+            return {
+                 type: document.querySelector(DOMstrings.inputType).value,  // will be either inc or exp
+                 description: document.querySelector(DOMstrings.inputDescription).value,
+                 value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
 
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    };
 })();
 
 
 // Global App Controller
 var controller = (function(budgetCtrl, UICtrl) {    
 
+    var DOM = UICtrl.getDOMstrings();
+
     var ctrlAddItem = function() {
+
         // 1. Get the field input data
+        var input = UICtrl.getinput();
+        console.log(input);
+
 
         // 2. Add the item to the budget controller
 
@@ -31,13 +54,12 @@ var controller = (function(budgetCtrl, UICtrl) {
 
         // 5. Display the budget on the UI
 
-        console.log('works');
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function(e) {
-        if (e.keyCode === 13 || e.whicH === 13) {
+        if (e.keyCode === 13 || e.which === 13) {
             ctrlAddItem();
         }
     });
